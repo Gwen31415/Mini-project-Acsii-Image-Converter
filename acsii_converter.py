3eafc638a73671d221a5b757046550df49305f55
Gwen <gwenael.lasserre@laposte.net>
2026-08-09 23:58:35 +0200

    Changed : Now verify if the file is compatible

--- acsii_converter.py
+++ acsii_converter.py
@@ -1,52 +1,104 @@
 from PIL import Image, ImageDraw, ImageFont
 from json import load
 
+def convert(img):
+
+    new_img_char = []
+
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 10)
+    bbox = font.getbbox("#")
+    char_w = bbox[2] - bbox[0]
+    char_h = bbox[3] - bbox[1]
+    ratio = char_h/char_w
+
+    new_height = round(img.size[1] / ratio)
+    img = img.resize((img.size[0], new_height))
+
+    width = img.size[0] * char_w
+    height = img.size[1] * char_h
+
+    new_img = Image.new("RGB", (width, height), "black")
+    draw = ImageDraw.Draw(new_img)
+
+    img_brightness = []
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            pixel = img.getpixel((x, y))
+            img_brightness.append(sum(pixel))
+
+    brighter = max(img_brightness)/(len(color_table)-1)
+    percent = 0
+    for y in range(img.size[1]):
+        row = ""
+        for x in range(img.size[0]):
+            i = x * img.size[1] + y
+            img_brightness[i] = round(img_brightness[i]/brighter)
+
+            row +=  color_table[img_brightness[i]]
+
+        draw.text((0, y  * char_h), row, font=font, fill="white")
+        new_img_char.append(row)
+
+    return new_img_char, new_img
+
+valid_image_files = [
+    "jpg",
+    "jpe",
+    "jpeg",
+    "jfif",
+    "png"
+    "apng",
+    "bmp",
+    "dib",
+    "gif",
+    "tif",
+    "tiff",
+    "webp",
+    "ico",
+    "icns",
+    "eps",
+    "ps",
+    "pbm",
+    "pgm",
+    "pnm",
+    "ppm",
+    "pcx",
+    "tga",
+    "icb",
+    "vda",
+    "vst",
+    "sgi",
+    "bw",
+    "rgb",
+    "rgba",
+    "xbm",
+    "dds",
+    "msp",
+    "wmf",
+    "emf",
+    "jp2",
+    "j2k",
+    "jpc",
+    "jpf",
+    "jpx",
+    "h5",
+    "hdf"
+]
+
 with open("palette.json") as file:
     color_table = load(file)
 
 file_name = input("Enter file name: ")
 
-img = Image.open(f"resources/{file_name}")
+if file_name.split(".")[-1] in valid_image_files:
+    img = Image.open(f"resources/{file_name}")
 
-new_img_char = []
+    new_img_char, new_img = convert(img)
+    new_img.save(f"resources/{file_name.split(".")[0]}_acsii.png")
 
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 10)
-bbox = font.getbbox("#")
-char_w = bbox[2] - bbox[0]
-char_h = bbox[3] - bbox[1]
-ratio = char_h/char_w
+    with open(f"resources/{file_name.split(".")[0]}.txt", "w") as file:
+        file.write("\n".join(new_img_char))
 
-new_height = round(img.size[1] / ratio)
-img = img.resize((img.size[0], new_height))
-
-width = img.size[0] * char_w
-height = img.size[1] * char_h
-
-new_img = Image.new("RGB", (width, height), "black")
-draw = ImageDraw.Draw(new_img)
-
-img_brightness = []
-for x in range(img.size[0]):
-    for y in range(img.size[1]):
-        pixel = img.getpixel((x, y))
-        img_brightness.append(sum(pixel))
-
-brighter = max(img_brightness)/(len(color_table)-1)
-percent = 0
-for y in range(img.size[1]):
-    row = ""
-    for x in range(img.size[0]):
-        i = x * img.size[1] + y
-        img_brightness[i] = round(img_brightness[i]/brighter)
-
-        row +=  color_table[img_brightness[i]]
-
-    draw.text((0, y  * char_h), row, font=font, fill="white")
-    new_img_char.append(row)
-
-new_img.save(f"resources/{file_name.split(".")[0]}_acsii.png")
-
-with open(f"resources/{file_name.split(".")[0]}.txt", "w") as file:
-    file.write("\n".join(new_img_char))
-
+else :
+    print("Invalid file name")
 
